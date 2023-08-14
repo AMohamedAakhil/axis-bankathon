a04991b35888d349110e134fed04824ca0815df9
fvviz <faizrahimbusiness@gmail.com>
2023-08-14 14:18:51 +0530

    finished cv ranking

--- src/server/py_utils/utils/cv_llm.py
+++ src/server/py_utils/utils/cv_llm.py
@@ -1,8 +1,20 @@
 import requests
 from pypdf import PdfReader
-import os
 from langchain.chains import LLMChain, SequentialChain
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+import json
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import asyncio
+
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 
 class ReadPDF:
@@ -23,35 +35,43 @@ class ReadPDF:
             
         return all_text
     
-class CVLLM:
-    def __init__(self, CV_list, job_title, job_description) -> None:
-        self.CV_list = CV_list
+class BaseCVLLM:
+    def __init__(self, cv, job_title, job_description) -> None:
+        self.cv = cv
         self.job_title = job_title
-        self.job_descripiton = job_description
-        self.llm = OpenAI(temperature=0.6, max_tokens=-1, openai_api_key=OPENAI_API_KEY)
+        self.job_description = job_description
+        self.llm = OpenAI(temperature=0.6, max_tokens=-1, 
+                          openai_api_key=OPENAI_API_KEY,
+                          #model="ada")
+        )
 
-        with open("src/server/py_utils/elements.json") as elements_file:
+        with open("src/server/py_utils/json_components/cv_elements.json") as elements_file:
                self.elements = json.load(elements_file)
+
+    async def async_generate(self, sqc, inputs):
+          resp = await sqc.arun(inputs)
+          return resp
+    
     async def build_dict(self):
           prompt1 = """
-          Given a CV and Job description for the Job title: {job_title},
+          Given a CV for the Job title: {job_title},
           Analayse the CV and identify the follwing component:
         
          {field}: {field_info}
 
-          Scan the description for the presence of this field, Identify the segment.
+          Scan the CV for the presence of this field, Identify the segment.
           ONLY Return a json formatted string where the property name is {field} and value is the identied segment
           If you cannot identify the segment, The value of the property should be null.
           -----
-          Job description :
-          {job_description}
-
+          CV:
+          {cv}
+          -----
 
           Make sure to RETURN ONLY JSON, not code or any other text. 
           """
 
           prompt1_template = PromptTemplate(
-               input_variables=["job_title" ,"job_description", "field", "field_info"],
+               input_variables=["job_title" ,"cv", "field", "field_info"],
                template= prompt1
           )
 
@@ -61,7 +81,7 @@ class CVLLM:
           
           dict_chain = SequentialChain(
                chains = [chain1],
-               input_variables = ["job_title", "job_description", "field", "field_info"],
+               input_variables = ["job_title", "cv", "field", "field_info"],
                output_variables = ["dictionary"],
                verbose = False
           )
@@ -73,7 +93,7 @@ class CVLLM:
                "job_title": self.job_title,
                "field": field,
                "field_info": field_info,
-               "job_description": self.job_description
+               "cv": self.cv
                }
 
                tasks.append(self.async_generate(dict_chain, inputs))
@@ -87,3 +107,143 @@ class CVLLM:
                
                elements_dict[name_] = temp_[name_]
           return elements_dict
+    
+    async def generate_concurrently(self):
+          pt = PromptTemplate(
+               input_variables=["job_title", "field", "field_info", "field_val", "job_description"],
+               template = """
+              Your team is evaluating CVs. Your job is to evaluate the {field} in a given CV.
+              See how well the provided {field} in this CV in this field is applicable for the job title {job_title}.
+              The job description of this post will also be provided to you, Use it as a reference to score the provided field: {field}
+              Based on this, Score the {field} out of 10. If the field has been listed as null, give the score as 0
+              You are allowed to use decimal values, Return ONLY THE SCORE AND NO OTHER TEXT.
+
+              {field} = {field_info}. 
+              ---
+              Job description:
+              {job_description}
+              ---
+              This is what has been provided in the applicants CV:
+
+              {field}:
+
+              {field_val}
+              ---         
+          """
+          )
+          chain = LLMChain(llm = self.llm,
+                           prompt=pt,
+                           output_key="score")
+          
+          sqc = SequentialChain(
+               chains = [chain],
+               input_variables = ["job_title", "field", "field_info", "field_val", "job_description"],
+               output_variables = ["score"],
+               verbose = False
+          )
+
+          elements_dict = await self.build_dict() 
+          tasks = []
+
+          for field in elements_dict.keys():
+               field_info = self.elements[field]
+               field_val = elements_dict[field]
+               inputs = {
+               "job_title": self.job_title,
+               "field": field,
+               "field_info": field_info,
+               "field_val": field_val,
+               "job_description": self.job_description
+               }
+
+               tasks.append(self.async_generate(sqc, inputs))
+
+          results = await asyncio.gather(*tasks)
+          results  = [float(i) for  i in results]
+
+          sum_score = sum(results)
+          return  sum_score, elements_dict
+
+class CVranker:
+     def __init__(self, cv_list: list, job_title: str, job_description: str) -> None:
+          self.cv_list = cv_list,
+          self.job_title = job_title,
+          self.job_description = job_description 
+          self.review_llm = OpenAI(temperature=0.6, openai_api_key=OPENAI_API_KEY)
+
+     def sort_func(self, cvs):
+          return cvs['score']
+     
+     def build_review_llm(self):
+          pt = PromptTemplate(
+               input_variables=["cv", "job_title"],
+               template = """
+                Given a CV for the the job title: {job_title}.
+                Summarise the CV in a short, clear and concise way. Make sure to retain all the important elements of the CV.
+                ---
+                CV:
+                {cv}        
+                ---
+                Return ONLY the short summarized CV.
+                """
+          )
+
+          llmchain = LLMChain(llm = self.review_llm,
+                              prompt=pt,
+                              output_key="review")
+          
+          review_llm = SequentialChain(
+               chains = [llmchain],
+               input_variables=["cv","job_title"],
+               output_variables=["review"],
+               verbose = False
+          )
+
+          return review_llm
+
+     async def async_generate_scores(self, cv_llm):
+          score, elements_dict = await cv_llm.generate_concurrently()
+          return score, elements_dict['Contact information']
+     
+     async def async_generate_reviews(self, review_llm, inputs):
+          resp = await review_llm.arun(inputs)
+          return resp
+
+     async def generate_scores(self):
+          score_tasks= []
+          review_tasks = []
+          for cv in self.cv_list[0]:
+               cv_score_llm  = BaseCVLLM(job_title=self.job_title,
+                                        job_description=self.job_description,
+                                        cv= cv)
+               review_llm = self.build_review_llm()
+
+               score_tasks.append(self.async_generate_scores(cv_score_llm))
+               review_tasks.append(self.async_generate_reviews(review_llm, 
+                                                              {"cv": cv, "job_title": self.job_title}))
+          results1 = await asyncio.gather(*score_tasks)
+          results2 = await asyncio.gather(*review_tasks)
+
+          cv_rankings = []
+          for num, result in enumerate(results1):
+               cv_info = {}
+               cv_info['score'] = result[0]
+               cv_info['contact_info'] = result[1]
+               cv_info['short_summary'] = results2[num]
+               cv_rankings.append(cv_info)
+
+          cv_rankings.sort(key=self.sort_func, reverse=True)
+          return cv_rankings
+          
+               
+            
+
+               
+               
+               
+
+          
+               
+               
+            
+          
