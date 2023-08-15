3a51406201757b075607535f8022def59e796082
fvviz <faizrahimbusiness@gmail.com>
2023-08-15 13:26:39 +0530

    added scoring function

--- src/server/py_utils/utils/interview_llm.py
+++ src/server/py_utils/utils/interview_llm.py
@@ -1,5 +1,5 @@
 import os
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 import os
@@ -37,7 +37,45 @@ class InterviewLLM:
           chain1 = LLMChain(llm = self.llm, prompt=prompt1_template, 
                             output_key="dictionary"
                         )
+          
           return chain1.run(job_title=self.job_title, job_description=self.job_description, CV=self.cv)
+     
+     def evaluate_answer(self, question, answer):
+          eval_prompt = PromptTemplate(
+               input_variables=["job_title" ,"question", "answer"],
+               template= """
+                You are a recruiter for a company. You are recruiting for the role of {job_title}. 
+                You have provided an interview question to candidate and they have given you the answer.
+                Your job is evaluate the answer and score it out of 10. You are allowed to use decimal values.
+                ---
+                The question given to candidate:
+                {question}
+                ---
+                The answer provided by the candidate:
+                {answer}
+                ---
+               Evaluate the answer Only return the score out of 10 and no other text.
+               """
+          )
+
+          eval_chain = LLMChain(llm = self.llm,
+                                prompt = eval_prompt,
+                                input_variables = ["job_title", "question", "answer"],
+                                output_key="score")
+          return eval_chain.run(job_title = self.job_title, 
+                                question = question,
+                                answer = answer)
+          
+
+          
+
+
+
+          
+          
+     
+
+
      
 
 
