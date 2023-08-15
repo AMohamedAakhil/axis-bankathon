09f15033d9678f17d5d1b1f35361634a8436225c
fvviz <faizrahimbusiness@gmail.com>
2023-08-15 20:41:32 +0530

    opps code added

--- src/server/py_utils/server.py
+++ src/server/py_utils/server.py
@@ -1,6 +1,13 @@
 import fastapi
-import asyncio
 import uvicorn
+import os
+
+import smtplib
+from email.message import EmailMessage
+import ssl
+
+from dotenv import load_dotenv
+from os.path import join, dirname
 
 from utils.job_desc_llm import JobDescLLM
 from utils.cv_llm import CVranker
@@ -8,6 +15,10 @@ from utils.interview_llm import InterviewLLM
 
 app = fastapi.FastAPI()
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+EMAIL_PASS = os.environ.get("EMAIL_PASS")
 
 @app.post("/job_desc_score/")
 async def job_desc_score_endpoint(data: dict):
@@ -76,6 +87,61 @@ async def evaluate_qna_endpoint(data: dict):
         return score_per_question, total_score
     except ValueError:
         return "We ran into an unexpected error, Please try again"
+    
+@app.post("/evaluate_qna/")
+async def evaluate_qna_endpoint(data: dict):
+    job_title = data.get('job_title')
+    question = data.get('questions')
+    answer = data.get('answers')
+
+    try:
+
+        score_per_question, total_score = await InterviewLLM.evaluate_answers(job_title=job_title,
+                                         question=question,
+                                         answer=answer)
+        return score_per_question, total_score
+    except ValueError:
+        return "We ran into an unexpected error, Please try again"
+    
+@app.post("/send_round1_email/")
+def send_round1_mail(data):
+    email_sender = 'axisbank.hrteam123@gmail.com'
+    email_password = EMAIL_PASS
+    email_receiver = data.get('receiver_email')
+    interview_link = data.get('interview_link')
+
+    subject = 'Congratulations on Your Selection for an Round 1 Interview at Axis Bank!'
+    body =f"""
+    Dear Candidate,
+    We are thrilled to inform you that after a thorough review of your application, we are impressed with your qualifications and experiences,
+    and we would like to invite you for an interview at Axis Bank.
+    Your application stood out among a competitive pool of candidates, and we believe your skills and background align well with what we are looking
+    for in this position.
+    
+    Interview Details:
+    Mode: Online
+    Link: {interview_link}
+    
+    Best regards,
+
+    Axis Bank
+    HR admin
+    """
+
+    em = EmailMessage()
+    em['From'] = email_sender
+    em['To'] = email_receiver
+    em['Subject'] = subject
+    em.set_content(body)
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
+    response={
+        "sent" : "true",
+    }
+    return response
 
 
 if __name__ == "__main__":
